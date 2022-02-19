5c140e9a3a6e5dc0a2df8db5e930f3f42330dbd7
sirasbook <siras_2543@hotmail.com>
2022-02-19 21:56:13 +0700

    use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Switch, useHistory } from "react-router-dom";
+import Request from "./request/request"
 
 function App() {
+
+  const history = useHistory();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact key="request" path="/" component={Request}></Route>
+      </Switch>
+    </Router>
   );
 }
 
