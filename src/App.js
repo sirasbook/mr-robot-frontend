6d8f9531fe368a9f0798719d3bf2c073f3f44f34
sirasbook <siras_2543@hotmail.com>
2022-02-25 20:19:40 +0700

    add all-report file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Router, Route, Switch, useHistory } from "react-router-dom";
 import Request from "./request/request"
+import Report from './reports/report';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Router history={history}>
       <Switch>
         <Route exact key="request" path="/" component={Request}></Route>
+        <Route exact key="report" path="/report" component={Report}></Route>
       </Switch>
     </Router>
   );
---
+++ src/reports/report.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TechnologyStack from "./technology-stack/technology";
+import "./report.css"
+
+const Report = () => {
+    return (
+        <div className="report-container">
+            <TechnologyStack/>
+        </div>
+    )
+}
+
+export default Report;
