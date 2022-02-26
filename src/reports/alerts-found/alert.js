8a52d20e7d6b0f0febcac8c65e8f18d7986052b7
sirasbook <siras_2543@hotmail.com>
2022-02-26 14:08:30 +0700

    create alert part in report

---
+++ src/reports/alerts-found/alert.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./alert.scss";
+
+const Alert = () => {
+    return (
+        <div className="alert-container">
+            <h2>Alert summary</h2>
+            <h2>Alert Details</h2>
+        </div>
+    )
+}
+
+export default Alert;
