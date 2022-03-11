481e0097a315533d643ff395c82d4f1c2c0002bb
sirasbook <siras_2543@hotmail.com>
2022-03-11 20:26:51 +0700

    hide summary part

--- src/reports/report.js
+++ src/reports/report.js
@@ -12,12 +12,12 @@ const Report = () => {
                 <h1>Summary of Scanning Result</h1>
                 <h2>Domain: {sessionStorage.getItem('url')}</h2>
                 <h2>This summary include:</h2>
-                <li><a href="#summary">Summary</a></li>
+                {/* <li><a href="#summary">Summary</a></li> */}
                 <li><a href="#technology">Technology-stack</a></li>
                 <li><a href="#subdomain">Subdomains</a></li>
                 <li><a href="#leaked-dir">Leaked directory</a></li>
                 <li><a href="#alert">Alert founded</a></li>
-                <h2>Summary</h2>
+                {/* <h2 id="summary">Summary</h2> */}
             </div>
             <TechnologyStack/>
             <Subdomain/>
