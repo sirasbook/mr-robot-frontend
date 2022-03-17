48fa0625e754e02b2dbdbf081573f154ad31c5ea
sirasbook <siras_2543@hotmail.com>
2022-03-17 14:03:08 +0700

    remove duplicated table

--- src/reports/technology-stack/technology.js
+++ src/reports/technology-stack/technology.js
@@ -5,6 +5,7 @@ import tempData from "../../data/technology_data.json";
 const TechnologyStack = () => {
     const [data, setData] = useState(tempData);
     let c = 0;
+    let check = new Set()
 
     const displayData = data.map(
         (info) => {
@@ -14,30 +15,34 @@ const TechnologyStack = () => {
                 (body) => {
                     return (
                         <tr>
-                            <td>
-                                <a href={`https://www.wappalyzer.com/technologies/${info.slug}/${body.slug}`}>
+                            <td className="flex-row">
+                                <a style={{marginRight: "10px"}} href={`https://www.wappalyzer.com/technologies/${info.slug}/${body.slug}`}>
                                     {body.name}
                                 </a>
+                                {body.version === null ? null : <div className="version">{body.version}</div>}
                             </td>
                         </tr>
                     )
                 }
             )
 
-            return (
-                <div>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>{info.name}</th>
-                            </tr>
-                        </thead>
-                        <tbody id={info.name}>
-                            {displayBody}
-                        </tbody>
-                    </table>
-                </div>
-            )
+            if (!(check.has(info.id))) {
+                check.add(info.id)
+                return (
+                    <div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>{info.name}</th>
+                                </tr>
+                            </thead>
+                            <tbody id={info.name}>
+                                {displayBody}
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
         }
     )
 
